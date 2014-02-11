fe70b855c2a464bdbdb1cb6cb9aa5c6ce94afe58
Yuriy Dyrenko <mail@yuriy.me>
2014-02-11 12:37:00 -0600

    Initial commit.

--- Project Barbosa/MainViewController.m
+++ Project Barbosa/MainViewController.m
@@ -6,18 +6,21 @@
 //  Copyright (c) 2014 Project Barbosa. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "MainViewController.h"
 
-@interface ViewController ()
+@interface MainViewController ()
 
 @end
 
-@implementation ViewController
+@implementation MainViewController
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    self.title = @"MainViewController";
+    
+    NSLog(@"MainViewController Loaded");
 }
 
 - (void)didReceiveMemoryWarning
