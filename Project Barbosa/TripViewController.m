1610a5a0aec79e27778dee2761b9a177c4c62fd1
yuriydyrenko <mail@yuriy.me>
2014-03-13 16:01:05 -0500

    UI Improvements

--- Project Barbosa/TripViewController.m
+++ Project Barbosa/TripViewController.m
@@ -123,7 +123,7 @@ static NSString *mapViewAnnotationIdentifier = @"TripViewMapAnnotation";
         [self.mapView addAnnotation:annotation];
     }
     
-    [self centerMapForCoordinate:coordinates[0]];
+    [self zoomMapViewToFitAnnotations:self.mapView animated:NO];
     
     MKPolyline *tripPath = [MKPolyline polylineWithCoordinates:coordinates count:self.trip.itinerary.count];
     [self.mapView addOverlay:tripPath];
@@ -135,7 +135,7 @@ static NSString *mapViewAnnotationIdentifier = @"TripViewMapAnnotation";
     
     if(pin == nil)
     {
-        pin =[[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:mapViewAnnotationIdentifier];
+        pin = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:mapViewAnnotationIdentifier];
     }
     
     pin.animatesDrop = NO;
@@ -143,6 +143,66 @@ static NSString *mapViewAnnotationIdentifier = @"TripViewMapAnnotation";
     pin.pinColor = MKPinAnnotationColorRed;
     
     return pin;
+}
+
+#define MINIMUM_ZOOM_ARC 0.014 //approximately 1 miles (1 degree of arc ~= 69 miles)
+#define ANNOTATION_REGION_PAD_FACTOR 2.5
+#define MAX_DEGREES_ARC 360
+//http://brianreiter.org/2012/03/02/size-an-mkmapview-to-fit-its-annotations-in-ios-without-futzing-with-coordinate-systems/
+- (void)zoomMapViewToFitAnnotations:(MKMapView *)mapView animated:(BOOL)animated
+{
+    CGPoint point;
+    CLLocationCoordinate2D coordinate;
+    NSArray *annotations = mapView.annotations;
+    NSInteger count = [mapView.annotations count];
+    if ( count == 0) { return; } //bail if no annotations
+     
+    //convert NSArray of id <MKAnnotation> into an MKCoordinateRegion that can be used to set the map size
+    //can't use NSArray with MKMapPoint because MKMapPoint is not an id
+    MKMapPoint points[count]; //C array of MKMapPoint struct
+    for( int i=0; i<count; i++ ) //load points C array by converting coordinates to points
+    {
+        coordinate = [(id <MKAnnotation>)[annotations objectAtIndex:i] coordinate];
+        points[i] = MKMapPointForCoordinate(coordinate);
+    }
+    //create MKMapRect from array of MKMapPoint
+    MKMapRect mapRect = [[MKPolygon polygonWithPoints:points count:count] boundingMapRect];
+    //convert MKCoordinateRegion from MKMapRect
+    MKCoordinateRegion region = MKCoordinateRegionForMapRect(mapRect);
+     
+    //add padding so pins aren't scrunched on the edges
+    region.span.latitudeDelta  *= ANNOTATION_REGION_PAD_FACTOR;
+    region.span.longitudeDelta *= ANNOTATION_REGION_PAD_FACTOR;
+    //but padding can't be bigger than the world
+    if( region.span.latitudeDelta > MAX_DEGREES_ARC ) { region.span.latitudeDelta  = MAX_DEGREES_ARC; }
+    if( region.span.longitudeDelta > MAX_DEGREES_ARC ){ region.span.longitudeDelta = MAX_DEGREES_ARC; }
+     
+    //and don't zoom in stupid-close on small samples
+    if( region.span.latitudeDelta  < MINIMUM_ZOOM_ARC ) { region.span.latitudeDelta  = MINIMUM_ZOOM_ARC; }
+    if( region.span.longitudeDelta < MINIMUM_ZOOM_ARC ) { region.span.longitudeDelta = MINIMUM_ZOOM_ARC; }
+    //and if there is a sample of 1 we want the max zoom-in instead of max zoom-out
+    if( count == 1 )
+    { 
+        region.span.latitudeDelta = MINIMUM_ZOOM_ARC;
+        region.span.longitudeDelta = MINIMUM_ZOOM_ARC;
+    }
+    
+    
+    point = [mapView convertCoordinate:region.center toPointToView:mapView];
+    point.x += 30;
+    point.y += 20;
+    region.center = [mapView convertPoint:point toCoordinateFromView:mapView];
+    
+    [mapView setRegion:region animated:animated];
+}
+
+- (void)moveCenterByOffset:(CGPoint)offset from:(CLLocationCoordinate2D)coordinate
+{
+    CGPoint point = [self.mapView convertCoordinate:coordinate toPointToView:self.mapView];
+    point.x += offset.x;
+    point.y += offset.y;
+    CLLocationCoordinate2D center = [self.mapView convertPoint:point toCoordinateFromView:self.mapView];
+    [self.mapView setCenterCoordinate:center animated:NO];
 }
 
 - (void)centerMapForCoordinate:(CLLocationCoordinate2D)coordinate
