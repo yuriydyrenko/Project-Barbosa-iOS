9f73fb666060dcffca14373e84f7bd157c798fb9
yuriydyrenko <mail@yuriy.me>
2014-03-15 03:32:45 -0500

    Rewriting Trip tests

--- Project BarbosaTests/Trip_Tests.m
+++ Project BarbosaTests/Trip_Tests.m
@@ -6,50 +6,45 @@
 //  Copyright (c) 2014 Project Barbosa. All rights reserved.
 //
 
-#import <XCTest/XCTest.h>
+#import "Kiwi.h"
 #import "Trip.h"
-#import "JSONModel.h"
 
-@interface Trip_Tests : XCTestCase
+SPEC_BEGIN(TripSpec)
 
-@end
-
-@implementation Trip_Tests
-
-- (void)setUp
-{
-    [super setUp];
-    // Put setup code here; it will be run once, before the first test case.
-}
-
-- (void)tearDown
-{
-    // Put teardown code here; it will be run once, after the last test case.
-    [super tearDown];
-}
-
-- (void)testIDAndName
-{
-    Trip *trip = [[Trip alloc] initWithID:@"1" name:@"Test Trip"];
+describe(@"Trip", ^{
+    it(@"should map a trip with no itinerary items correctly", ^{
+        Trip *trip = nil;
+        NSDictionary *tripDictionary = nil;
+        NSString *tripJSONString = @"{\"_id\": \"5323373703ebc20200ce35df\", \"__v\": 0, \"archived\": false, \"date\": \"2014-03-14T17:07:03.719Z\", \"location\": \"London\", \"user\": \"Jules Verne\", \"itinerary\": [], \"name\": \"Around the World in Eighty Days\"}";
+        NSData *tripData = [tripJSONString dataUsingEncoding:NSUTF8StringEncoding];
+        NSError *error = nil;
+        
+        [[tripData shouldNot] beNil];
+        tripDictionary = [NSJSONSerialization JSONObjectWithData:tripData options:0 error:&error];
+        [[error should] beNil];
+        trip = [[Trip alloc] initWithDictionary:tripDictionary error:&error];
+        [[trip shouldNot] beNil];
+        [[error should] beNil];
+        
+        [[trip._id should] equal:@"5323373703ebc20200ce35df"];
+        [[trip.name should] equal:@"Around the World in Eighty Days"];
+        [[trip.itinerary should] beEmpty];
+    });
     
-    XCTAssertEqual(trip._id, @"1", @"Trip ID not equal");
-    XCTAssertEqual(trip.name, @"Test Trip", @"Trip name not equal.");
-    
-    XCTAssertNotEqual(trip._id, @"2", @"Trip ID error.");
-    XCTAssertNotEqual(trip.name, @"Test Trip 2", @"Trip name not equal.");
-}
+    it(@"should error while mapping a trip with no id", ^{
+        Trip *trip = nil;
+        NSDictionary *tripDictionary = nil;
+        NSString *tripJSONString = @"{\"__v\": 0, \"archived\": false, \"date\": \"2014-03-14T17:07:03.719Z\", \"location\": \"London\", \"user\": \"Jules Verne\", \"itinerary\": [], \"name\": \"Around the World in Eighty Days\"}";
+        NSData *tripData = [tripJSONString dataUsingEncoding:NSUTF8StringEncoding];
+        NSError *error = nil;
+        
+        [[tripData shouldNot] beNil];
+        tripDictionary = [NSJSONSerialization JSONObjectWithData:tripData options:0 error:&error];
+        [[error should] beNil];
+        trip = [[Trip alloc] initWithDictionary:tripDictionary error:&error];
+        [[trip should] beNil];
+        [[error shouldNot] beNil];
+    });
+});
 
-- (void)testJSONModelDictionaryKeyMapping
-{
-    NSError *error;
-    NSDictionary *tripDictionary = @{@"_id" : @"2", @"name" : @"Test Trip 2"};
-    Trip *trip = [[Trip alloc] initWithDictionary:tripDictionary error:&error];
-    
-    XCTAssertEqual(trip._id, @"2", @"Trip ID not equal");
-    XCTAssertEqual(trip.name, @"Test Trip 2", @"Trip name not equal.");
-    
-    XCTAssertNotEqual(trip._id, @"1", @"Trip ID error.");
-    XCTAssertNotEqual(trip.name, @"Test Trip", @"Trip name error.");
-}
-
-@end
+SPEC_END
